cf6efa2a44178741f5ddfd8b86ccc65ea682ac0f
RashedAbdullah <maarashed40@gmail.com>
2024-09-23 00:36:39 +0600

    Some changes about resume

--- app/[lang]/_components/navbar.jsx
+++ app/[lang]/_components/navbar.jsx
@@ -7,22 +7,24 @@ import MobileNav from "./mobile-nav";
 
 const Navbar = ({ lang }) => {
   return (
-    <header
-      dir={lang === "ar" ? "rtl" : "ltr"}
-      className="container border-b py-6 flex justify-between items-center align-middle sticky top-0 z-50 backdrop-blur-lg"
-    >
-      <Logo lang={lang} />
-      <div className="hidden lg:flex gap-10 justify-center items-center align-middle">
-        <Navigations lang={lang} />
-        <NavSearch lang={lang} />
-        <SocialLinks lang={lang} />
-        <LanguageSwitcher lang={lang} />
-      </div>
+    <div className="border-b-[0.1px] sticky top-0 z-50">
+      <header
+        dir={lang === "ar" ? "rtl" : "ltr"}
+        className="container py-6 flex justify-between items-center align-middle backdrop-blur-lg"
+      >
+        <Logo lang={lang} />
+        <div className="hidden lg:flex gap-10 justify-center items-center align-middle">
+          <Navigations lang={lang} />
+          <NavSearch lang={lang} />
+          <SocialLinks lang={lang} />
+          <LanguageSwitcher lang={lang} />
+        </div>
 
-      <div className="lg:hidden inline-block">
-        <MobileNav lang={lang} />
-      </div>
-    </header>
+        <div className="lg:hidden inline-block">
+          <MobileNav lang={lang} />
+        </div>
+      </header>
+    </div>
   );
 };
 
