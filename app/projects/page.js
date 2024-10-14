ba235dec8ccfa7984b3ea339d54d0f1d1063ba60
RashedAbdullah <maarashed40@gmail.com>
2024-10-14 22:13:51 +0600

    Some small errors fixed

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,10 +1,14 @@
-import { ItemsCard } from "@/components/ui/card-hover-effect";
 import { projects } from "@/data/projects";
+import ProjectCard from "./_components/project-card";
 
 const ProjectPage = () => {
   return (
     <div className="container min-h-screen">
-      <ItemsCard items={projects} />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10">
+        {projects.map((project, idx) => (
+          <ProjectCard key={idx} project={project} />
+        ))}
+      </div>
     </div>
   );
 };
--- components/ui/card-hover-effect.jsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import Link from "next/link";
-import { Button } from "./button";
-import ItemCard from "@/app/blogs/_components/blog-card";
-
-export const ItemsCard = ({ items }) => {
-  return <ItemCard key={idx} item={item} />;
-};
