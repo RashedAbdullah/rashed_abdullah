04f51818a9768f81a777157c18efd8dc6d528869
RashedAbdullah <maarashed40@gmail.com>
2025-01-06 16:10:56 +0600

    some backend updates for mobile app

--- app/api/diraies/route.js
+++ app/api/diraies/route.js
@@ -1,29 +1,54 @@
 import { mongoDBConnection } from "@/database/mongoDBConnection";
 import { dirayModel } from "@/models/diary-model";
 import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 
-export const GET = async () => {
+export const GET = async (req) => {
   try {
+    const url = new URL(req.url);
+    // Get the selected year from the query parameters
+    const year = url.searchParams.get("year");
+
+    // Validate the year parameter
+    if (!year || isNaN(year) || year.length !== 4) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Invalid year parameter. Please provide a valid year.",
+        },
+        { status: 400 }
+      );
+    }
+
     // Establish database connection
     await mongoDBConnection();
 
-    // Fetch all diraies
-    const diraies = await dirayModel.find({});
+    // Fetch all diaries for the selected year, sorted by the latest date
+    const diaries = await dirayModel
+      .find({
+        $expr: {
+          $eq: [
+            { $year: { $dateFromString: { dateString: "$date" } } },
+            parseInt(year),
+          ],
+        },
+      })
+      .sort({ date: -1 }); // Sorting by latest date
 
-    // Return success response with diraies data
+    // Return success response with diaries data
     return NextResponse.json({
       success: true,
-      data: diraies,
+      data: diaries,
     });
   } catch (err) {
     // Log error for debugging purposes
-    console.error("Error fetching diraies:", err);
+    console.error("Error fetching diaries:", err);
 
     // Return error response
     return NextResponse.json(
       {
         success: false,
-        message: "Failed to fetch diraies. Please try again later.",
+        message: "Failed to fetch diaries. Please try again later.",
         error: err.message || "An unexpected error occurred.",
       },
       { status: 500 }
