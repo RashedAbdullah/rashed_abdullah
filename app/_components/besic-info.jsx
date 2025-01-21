75ec40fa353c1149d0ea533fcb0b53f739fee2ba
RashedAbdullah <maarashed40@gmail.com>
2025-01-21 14:19:35 +0600

    info about me has been updated

--- app/_components/besic-info.jsx
+++ app/_components/besic-info.jsx
@@ -41,34 +41,12 @@ const BesicInfo = async () => {
             <div className="col-span-1 dark:text-aqua text-deepAqua">
               <GiRotaryPhone />
             </div>
-            <div className="col-span-7">+৮৮০১৬০৩-৪৪৩২১৪</div>
+            <div className="col-span-7">+8801603-443214</div>
 
             <div className="col-span-1 dark:text-aqua text-deepAqua">
               <FaLink />
             </div>
             <div className="col-span-7">www.rashedabdullah.com</div>
-          </div>
-
-          <div className="flex justify-evenly items-center text-white dark:text-slate-900">
-            <div className="dark:bg-aqua bg-deepAqua py-1 px-3 rounded-xl">
-              <p>JS</p>
-            </div>
-            <div className="dark:bg-aqua bg-deepAqua py-1 px-3 rounded-xl">
-              <p>Next.js</p>
-            </div>
-            <div className="dark:bg-aqua bg-deepAqua py-1 px-3 rounded-xl">
-              <p>Node.js</p>
-            </div>
-          </div>
-
-          <div className="flex justify-center">
-            <a
-              href="/RashedAbdullahResume.pdf"
-              download="RashedResume.pdf"
-              className="bg-white shadow text-deepBlack hover:text-white hover:bg-deepAqua transition-all duration-300 px-10 py-2 flex items-center gap-2 text-lg"
-            >
-              রিজিউমি <BiDownload />
-            </a>
           </div>
         </div>
       </div>
