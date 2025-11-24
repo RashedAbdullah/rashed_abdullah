45c679597e34f39e366b02fa8ff6227145c4c8a0
RashedAbdullah <maarashed40@gmail.com>
2025-11-24 17:25:56 +0600

    new version of this project

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -10,6 +10,7 @@ import {
   ThemeTogglerButton,
   type ThemeTogglerButtonProps,
 } from "@/components/animate-ui/components/buttons/theme-toggler";
+import Link from "next/link";
 
 interface ThemeTogglerButtonDemoProps {
   variant: ThemeTogglerButtonProps["variant"];
@@ -41,20 +42,74 @@ const Header = () => {
           transition={{ duration: 0.35, ease: "easeInOut" }}
           className="fixed top-10 left-1/2 -translate-x-1/2 z-50"
         >
-          <header className="flex gap-1">
+          <header className="flex gap-1 font-sf-mono">
             <a
               target="_blank"
               href="https://github.com/RashedAbdullah"
-              className="p-3 bg-secondary dark:bg-neutral-800 rounded-full backdrop-blur"
+              className="p-3 bg-secondary dark:bg-neutral-800 rounded-full backdrop-blur hover:bg-primary hover:text-white transition-colors"
             >
               <Github />
             </a>
 
-            <div className="flex items-center gap-20 py-3 px-6 rounded-lg bg-secondary dark:bg-neutral-800 backdrop-blur">
-              <h1 className="tracking-wide font-sf-mono">RASHED ABDULLAH</h1>
+            <div className="flex items-center gap-20 py-3 px-6 rounded-lg bg-secondary dark:bg-neutral-800 backdrop-blur relative">
+              <h1 className="tracking-wide">RASHED ABDULLAH</h1>
               <button onClick={() => setIsOpenMenu(!isOpenMenu)}>
                 {isOpenMenu ? <X /> : <Menu />}
               </button>
+              {isOpenMenu && (
+                <div className="absolute top-14 right-0 left-0">
+                  <ul className="text-lg w-full bg-secondary rounded-lg shadow-sm">
+                    <li>
+                      <Link
+                        href="/"
+                        className="block p-2 hover:bg-gray-800 hover:text-white"
+                      >
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="/"
+                        className="block p-2 hover:bg-gray-800 hover:text-white"
+                      >
+                        About
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="/services"
+                        className="block p-2 hover:bg-gray-800 hover:text-white"
+                      >
+                        Services
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="/"
+                        className="block p-2 hover:bg-gray-800 hover:text-white"
+                      >
+                        Skills
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="/"
+                        className="block p-2 hover:bg-gray-800 hover:text-white"
+                      >
+                        Projects
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="/"
+                        className="block p-2 hover:bg-gray-800 hover:text-white"
+                      >
+                        Contact
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              )}
             </div>
           </header>
         </motion.div>
